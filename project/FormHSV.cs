6d33632f73ae7dafd47815888955c4c959b2ce0b
Andrew Vaimon <niko142000@mail.ru>
2021-09-20 11:11:29 +0300

    first step to color adjustment

--- project/FormHSV.cs
+++ project/FormHSV.cs
@@ -12,9 +12,15 @@ namespace project
 {
     public partial class FormHSV : Form
     {
-        double averageHue;
+        int averageHue;
         double averageSaturation;
         double averageValue;
+
+        int currentHue;
+        double currentSaturation;
+        double currentValue;
+
+        bool isLoaded = false;
 
         private class HSV
         {
@@ -30,6 +36,44 @@ namespace project
                 this.hue = hue;
                 this.saturation = saturation;
                 this.value = value;
+            }
+
+            public void add(int hue, double sat, double val)
+            {
+                if(this.hue + hue < 0)
+                {
+                    this.hue = 360 + this.hue - hue;
+                }
+                else
+                {
+                    this.hue = (this.hue + hue) % 360;
+                }
+
+                if(saturation + sat < 0)
+                {
+                    saturation = 0;
+                } else if (saturation + sat > 100)
+                {
+                    saturation = 100;
+                }
+                else
+                {
+                    saturation += sat;
+                }
+
+                if (value + val < 0)
+                {
+                    value = 0;
+                }
+                else if (value + val > 100)
+                {
+                    value = 100;
+                }
+                else
+                {
+                    value += sat;
+                }
+
             }
         }
 
@@ -86,7 +130,7 @@ namespace project
         private RGB HSVtoRGB(double hue, double saturation, double value)
         {
             int sw = (int)Math.Floor(hue / 60) % 6;
-            double vmin = ((100 - saturation) * value) / 100;
+            double vmin = ((100 - saturation) * value) / 100.0;
             double a = (value - vmin) * ((hue % 60) / 60);
             double vinc = vmin + a;
             double vdec = value - a;
@@ -106,7 +150,7 @@ namespace project
         {
             bitmap = new Bitmap(imagePath);
 
-            var res = RGBtoHSV(127, 100, 14);
+            var res = HSVtoRGB(120, 60, 40);
             
             using (var fbitmap = new FastBitmap(bitmap))
             {
@@ -122,29 +166,66 @@ namespace project
                         value += (int)hsv.value;
                     }
                 }
-                averageHue = hue / fbitmap.Count;
-                averageSaturation = saturation / fbitmap.Count;
-                averageValue = value / fbitmap.Count;
+                currentHue = averageHue = (int) hue / fbitmap.Count;
+                currentSaturation = averageSaturation = saturation / fbitmap.Count;
+                currentValue = averageValue = value / fbitmap.Count;
             }
-            trackBarHue.Value = (int)averageHue;
+            trackBarHue.Value = averageHue;
             trackBarSaturation.Value = (int)averageSaturation;
             trackBarValue.Value = (int)averageValue;
+            pictureBox.Image = bitmap;
+            isLoaded = true;
+        }
+
+        private void updateImage(int hueShift = 0, double satShift = 0, double valShift = 0)
+        {
+            Bitmap bitmap = (Bitmap) pictureBox.Image;
+            using (var fbitmap = new FastBitmap(bitmap))
+            {
+                for (int x = 0; x < fbitmap.Width; x++)
+                {
+                    for (int y = 0; y < fbitmap.Height; y++)
+                    {
+                        var point = new Point(x, y);
+                        Color pixel = fbitmap.GetPixel(point);
+                        var hsv = RGBtoHSV(pixel.R, pixel.G, pixel.B);
+                        hsv.add(hueShift, satShift, valShift);
+                        var rgb = HSVtoRGB(hsv.hue, hsv.saturation, hsv.value);
+                        fbitmap.SetPixel(point, Color.FromArgb(rgb.r, rgb.g, rgb.b));
+                    }
+                }
+            }
             pictureBox.Image = bitmap;
         }
 
         private void trackBarHue_ValueChanged(object sender, EventArgs e)
         {
-
+            if (!isLoaded)
+            {
+                return;
+            }
+            updateImage(hueShift: trackBarHue.Value - currentHue);
+            currentHue = trackBarHue.Value;
         }
 
         private void trackBarSaturation_ValueChanged(object sender, EventArgs e)
         {
-
+            if (!isLoaded)
+            {
+                return;
+            }
+            updateImage(satShift: trackBarSaturation.Value - currentSaturation);
+            currentSaturation = trackBarSaturation.Value;
         }
 
         private void trackBarValue_ValueChanged(object sender, EventArgs e)
         {
-
+            if (!isLoaded)
+            {
+                return;
+            }
+            updateImage(valShift: trackBarValue.Value - currentValue);
+            currentValue = trackBarValue.Value;
         }
     }
 }
