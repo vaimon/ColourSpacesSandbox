ce717ca000b396078d83c33f0ac1abd1c98692a4
Elena <varlamova.elena200@gmail.com>
2021-09-22 21:52:56 +0300

    Add some comments

--- project/FormGray.cs
+++ project/FormGray.cs
@@ -32,14 +32,14 @@ namespace project
         }
         void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            coef = (int)comboBox1.SelectedItem;
+            coef = (int)comboBox1.SelectedItem;//считываем коэффициент для разности полутоновых изображений
             
         }
-        static Bitmap GoToGrey(Bitmap input,int flag)
+        static Bitmap GoToGrey(Bitmap input,int flag)//flag- номер применяемой формулы
         {
             Bitmap res = new Bitmap(input.Width, input.Height);
             double r, g, b;
-            using (var fbitmap = new FastBitmap(input))
+            using (var fbitmap = new FastBitmap(input))//битмап по исходной картинке
             {
                 for (int i = 0; i < fbitmap.Width; i++)
                 {
@@ -47,7 +47,7 @@ namespace project
                     {
                         Color color = fbitmap.GetPixel(new Point(i, j)); // FastBitmap для чтения текущего пикселя
                         if (flag == 1)
-                        {// Y'=0.299R+0.587G+0.114B
+                        {// Y'=0.299R+0.587G+0.114B первая формула
                              r = (color.R * 0.299);
                              g = (color.G * 0.587);
                              b = (color.B * 0.114);
@@ -55,13 +55,13 @@ namespace project
                         }
                         else 
                         {
-                            //Y'=0.2126R+0.7152G+0.0722B
+                            //Y'=0.2126R+0.7152G+0.0722B вторая формула
                             r = (color.R * 0.2126);
                             g = (color.G * 0.7152);
                             b = (color.B * 0.0722);
                             
                         }
-                        int gray = (int)(r + g + b);
+                        int gray = (int)(r + g + b);//считаем значение цвета
                         if (gray > 255)
                             gray = 255;
                         res.SetPixel(i, j, Color.FromArgb(gray,gray,gray));
@@ -76,40 +76,43 @@ namespace project
             Bitmap res = new Bitmap(input1.Width, input1.Height);
             
             int a,r, g, b,col;
-            var fbitmap2 = new FastBitmap(input2);
+            var fbitmap2 = new FastBitmap(input2);// FastBitmap для чтения текущего пикселя
             using (var fbitmap1 = new FastBitmap(input1))
             {
                 for (int i = 0; i < fbitmap1.Width; i++)
                 {
                     for (int j = 0; j < fbitmap1.Height; j++)
                     {
-                        Color color1 = fbitmap1.GetPixel(new Point(i, j)); // FastBitmap для чтения текущего пикселя
+                        Color color1 = fbitmap1.GetPixel(new Point(i, j)); 
                         Color color2 = fbitmap2.GetPixel(new Point(i, j));
 
                            // a = Math.Abs( color2.A - color1.A);
-                            r = Math.Abs(color2.R -color1.R);
+                            r = Math.Abs(color2.R -color1.R);//Находим разницу, значение интенсивности в каждом цвете тут одинаковое,
+                                                             //поэтому не так важно, какой цвет брать
                            // g = Math.Abs(color2.G -color1.G);
                             //b = Math.Abs(color2.B -color1.B);
 
-                        if (r * c > 255)
+                        if (r * c > 255)//коэффициент считывается из combobox, проверка, что с ним мы не выйдем за 255
                              col = 255;
                         else
                             col = r * c;
 
                         
-                        res.SetPixel(i, j, Color.FromArgb(col,col,col));
+                        res.SetPixel(i, j, Color.FromArgb(col,col,col));//закрашиваем пиксель
                     }
                 }
             }
             return res;
         }
-        static Bitmap Hist(Bitmap input)
+        static Bitmap Hist(Bitmap input)//рисуем гистограмму
         {
-            Dictionary<int, int> intense = new Dictionary<int, int>();
+            Dictionary<int, int> intense = new Dictionary<int, int>();//словарь
+            //ключ- интенсивность пикселя
+            //значение- количество пикселей данной интенсивности
             Bitmap res = new Bitmap(input.Width, input.Height);
             for (int i = 0; i < 256; i++)
             {
-                intense.Add(i, 0);
+                intense.Add(i, 0);//просто инициализируем и пока значения ключей пустые
             }
             using (var fbitmap = new FastBitmap(input))
             {
@@ -117,7 +120,9 @@ namespace project
                 {
                     for (int j = 0; j < fbitmap.Height; j++)
                     {
-                        Color color = fbitmap.GetPixel(new Point(i, j)); 
+                        Color color = fbitmap.GetPixel(new Point(i, j)); //считываем пиксель, т к у нас полутоновое изображение,
+                                                                         //тут все цвета будут иметь одинаковую насыщенность,
+                                                                         //все равно, по какому будем считать
                         //int x = color.R;
                            
                         if (intense.ContainsKey(color.R))
@@ -147,29 +152,30 @@ namespace project
                     }
                 }
             }
-            for (int i = 0; i < 256; i++)
+            for (int i = 0; i < 256; i++)//находим максимум пикселей интенсивности
             {
                 if (intense[i] > max)
                     max = intense[i];
             }
-            double point = (double)max / 256;
+            double scale = ((double)max )/ 256;//это для масштаба, вдруг у нас 1000000 пикселей этой интенсивности
 
-            for (int i = 0; i < 256; ++i)
+            for (int i = 0; i < 256; ++i)//по абсциссам 0..255
             {
-                for (int j = input.Height-1; j > 256 - intense[i] / point; --j)
+                for (int j = input.Height-1; j > 256 - intense[i] / scale; --j)//по ординатам закрашиваем нужное количество пикселей
+                                                                               //данной интенсивности
                     res.SetPixel(i, j, Color.DarkGray);
 
                 
             }
             return res;
         }
-        private void FormGray_Load(object sender, EventArgs e)
+        private void FormGray_Load(object sender, EventArgs e)//открываем исходную картиночку 
         {
             pictureBox.Load(imagePath);
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)//событие-при нажатии на кнопку запускаем перевод в серый по 1 формуле
         {
             Image newImage = Image.FromFile(imagePath);
             var bitmap = new Bitmap(newImage);
@@ -178,7 +184,7 @@ namespace project
             //res.Save("1", System.Drawing.Imaging.ImageFormat.Jpeg);
 
         }
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)//перевод в серый по второй формуле
         {
             Image newImage = Image.FromFile(imagePath);
             var bitmap = new Bitmap(newImage);
@@ -187,7 +193,7 @@ namespace project
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)//смотрим разницу
         {
             //Image newImage = Image.FromFile(imagePath);
             var bitmap1 = new Bitmap(pictureBox1.Image);
@@ -197,14 +203,14 @@ namespace project
             pictureBox3.Image = res;
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)//рисуем гистограмму для 1 полутонового изображения
         {
             var bitmap = new Bitmap(pictureBox1.Image);
             var res = Hist(bitmap);
             pictureBox4.Image = res;
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e)//рисуем гистограмму для 2 полутонового изображения
         {
             var bitmap = new Bitmap(pictureBox2.Image);
             var res = Hist(bitmap);
